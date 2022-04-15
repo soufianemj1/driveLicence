54096bea7dc1a406e6463949d91103a46a797ef2
soufianemj1 <elmajati.s@gmail.com>
2022-04-15 15:13:57 +0000

    redirections

--- jsquiz.js
+++ jsquiz.js
@@ -158,6 +158,9 @@
                    if(numCorrect>=3){
                     window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
                    }
+                   else{
+                    window.location.href = "https://c.tenor.com/MObnnZhtfb4AAAAC/what-a-loser-will-ferrel.gif";
+                   }
 
       return score;
     }
